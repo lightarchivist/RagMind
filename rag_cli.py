8296936b9a7836537e4783f8c8386eeb5336bf1c
Seb <50360059+lightarchivist@users.noreply.github.com>
2026-04-28 20:43:47 +0100

    Create rag_cli.py

---
+++ rag_cli.py
@@ -0,0 +1,9 @@
+# rag_cli.py — Universal Document Q&A CLI
+# Copyright (C) 2026  Seb Szmytka
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# See <https://www.gnu.org/licenses/> for details.
